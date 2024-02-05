c3c3a5b5f759ecc43561af369210c078290386f5
Ameneh-KM <ameneh.keshavarz@gmail.com>
2024-02-05 17:50:02 +0000

    first commit

---
+++ SwapFirstandLast/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+        myDLL.append(2);
+        myDLL.append(3);
+        myDLL.append(4);
+        myDLL.append(5);
+        
+        System.out.println("DLL before swap:");
+        myDLL.printList();
+        
+        myDLL.swapFirstLast();
+        
+        System.out.println("\nDLL after swap:");
+        myDLL.printList();
+
+    }
+}
